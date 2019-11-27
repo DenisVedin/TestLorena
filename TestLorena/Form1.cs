54816b7d9ecd8b5e8613cc216f97c7115486c186
DanteShade <shadow_94-94@mail.ru>
2019-11-27 17:32:30 +0500

    Добавлены функции соединения с базой данных и выполнения запросов

--- TestLorena/Form1.cs
+++ TestLorena/Form1.cs
@@ -7,11 +7,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SQLite;
 
 namespace TestLorena
 {
     public partial class FormTest : Form
     {
+        
+        private SQLiteConnection sql_con;
+        private SQLiteCommand sql_cmd;
+        private SQLiteDataAdapter DB;
+
+        //set connection
+        private void SetConnection()
+        {
+            sql_con = new SQLiteConnection("Data Source=TestLorena.db;Version=3;New=False;Compress=True;");
+        }
+
+        //set execute query
+        private void ExecuteQuery(string txtQuery)
+        {
+            SetConnection();
+            sql_con.Open();
+            sql_cmd = sql_con.CreateCommand();
+            sql_cmd.CommandText = txtQuery;
+            sql_cmd.ExecuteNonQuery();
+            sql_con.Close();
+        }
+
         public FormTest()
         {
             InitializeComponent();
